6a34c96b9ec063fbb3a74aa4ebdd36309201b123
curtisokeeffe <145132351+curtisokeeffe@users.noreply.github.com>
2023-12-09 12:47:26 -0500

    Final Project Complete
    Final Project

--- README.md
+++ README.md
@@ -1,2 +1,86 @@
-# FinalProject
-Stock API for project
+# Project Name
+
+StockInfo
+
+## Team Members
+- Curtis O'Keeffe
+
+## 1. Project Overview
+My Web application, **StockInfo**, is a stock information site that allows users to view key information about stocks of their choice. Features include dynamic charts with various time ranges, current price display, and market cap information, all accessible through an easy-to-navigate interface.
+
+### Target Audience
+Individuals who need quick access to basic stock metrics without the hassle of account sign-ups or ad interruptions.
+
+### Standout Aspect
+Focuses on essential information, offering a simpler alternative to information-heavy sites.
+
+## 2. Usage Guidelines
+To use **StockInfo**, run it locally as it is not yet deployed. Users can browse featured stocks or search for any publicly listed stock using its symbol.
+
+### Usage Steps
+- Enter the stock symbol (e.g., "MSFT" or "msft", but not "Microsoft")
+- Note: The API allows a maximum of 25 requests per day.
+
+## 3. Dependencies
+- **Flask**: A micro web framework for building web applications.
+- **DateTime**: For manipulating dates and times.
+- **Alpha Vantage API**: For fetching financial market data.
+- **JSON**: Built-in module for handling JSON data.
+- **Pandas**: For data manipulation and analysis.
+- **Bootstrap**: For creating the navigation bar.
+- **config**: For storing API keys.
+
+## 4. Project Structure
+The structure of **StockInfo** is outlined as follows:
+- `FinalProject`
+    - `Main`
+        - `app.py` (Main programming file)
+        - `config.py` (API key storage)
+        - `requirements.txt` (Deployment dependencies, not used)
+        - `__init__.py` (Deployment purposes, not used)
+        - `.gitignore` (Standard)
+        - `README.md` (Project information)
+        - `__pycache__` (Fast access data storage)
+        - `Templates` (HTML files)
+            - `index.html` (Homepage)
+            - `search_result.html` (Search result page)
+            - [Other HTML files for individual stocks]
+
+## 5. Collaboration Information
+No collaboration.
+
+## 6. Acknowledgments
+- **Alpha Vantage API**: For stock information.
+- **Bootstrap**: For navigation bar design.
+- **Pandas**: For data manipulation.
+- **Datetime**: For date handling.
+- **ChatGPT**: For debugging assistance.
+- **GitHub Copilot**: For code writing and debugging.
+- **GitHub**: For information and repository hosting.
+
+## 7. Reflection
+### Process Point of View
+I found that the process of developing this project was successful for the most past, as i met and exceeded my initial expectation in terms of final functionality and presentation of the site. The process of fetching the Alpha Vantage API information, converting it into a usable format and presenting it on a chart was easier then expected, but it was far from complete. 
+
+initially, the data was presented backwards, from newest to oldest left to right, which caused the current price to be the oldest datapoint for the selected time period. this was an issue as it gave inaccurate data and the graph was presented incorrectly. once this was fixed, i noticed issues with the quantity of data. there was a fixed amount of data that was being used for all the date ranges, which cuased the 1 year range to show far more info then the 1 day range, which may have only had 1 or 2 data points. to fix this, i used interday information, to fetch the stock price at smaller intervals such as every 5 minutes or 120 minutes. This fixed the presentation of the information and was a constantc challenge i was working on.
+
+Another main challenge was the API useage cap that i would keep running into. this was 25 requests a day and made it hard for me to work on the project for more then one hour. i tried to fix this by importing the API data into a static file, but once i started fetching data at 5 min intervals, this became unrealistic and my testing had decreased so i was not hitting my limit.
+
+For the formatting and presentation of the site, i wanted to make something simple yet professional. i did not want my site to look like something built by someone with no UI design knowledge so i utilised ChatGPT to create an outline for a professional looking simple theme, using a transparent blue background across the site, with text formated correctly and the navigaion bar adding the functionallity of the typical web application. 
+
+For testing the project, i would log data at different stages through out the process, to identify where issues occured. for example, i would log the quantity of data when it was fetched, transformed and passed to different routes and edited for the different ranges. this allowed me to see if the data quantity issue was due to fetching, manipulating or passing allowing me to fix it. This allowed me to catch errors that even ChatGPT and Copilot were unable to identify.
+
+I was happy with the functionality as i initially decided to only preset 5 stocks as well as have a fixed time range however once i reached this goal with plenty of time to spare, i started adding additional features such as the time range feature and the search feature as i felt this made the site a lot more practical and useful.
+
+The main change i would make if i where to continue working on this project would to deploy it so it can be used by everyone. i tried this early on, but struggled to get it to run and figured i would continue working on the functionallity before deploying. 
+
+
+### Learning Perspective
+
+Through the completion of this project, i was able to integrate my existing computer science knowledge to building a web application, which is the area in computer science that i have had the least experience with. I found that as soon as i understood the structure of the project, and the how flask worked, i was really able to utilise my existing knowledge to quickly build a functional site. This made it alot easier for me, and allowed me to relise how easy it is to learn new programming concpets and uses if you have an existing foundational knowledge.
+
+ChatGPT assisted greatly in the inital structuring and set up of my project, as i gave it a run down on my idea and asked it to create a setp by step guide that i could use to get my project started, as i was a bit unsure where to start. I also used it to diagnose errors, as it was much quicker then me looking through the lines of code i suspected was the issue. this was not always reliable and i found that i was having to use my own knowledge at least partially most of the time. I also used Github Copilot, which was extremely useful at predicting what i would type, allowing me to just have to strat each line. i would also use it to make changes on multiple routes so i would not have to do each route by hand. for example, i would give it amazons route and tell it to change all the other stocks routes to adapt the same functionallity. 
+
+Looking forward, i am keen to use the skills i have learnt whenever possible. this may be in the form of a personal project, or in internships as i am looking more in the tech field. Additionally, this project allowed me to give an example of my programming skills, which is beneficial for future employment oportunities. Furthermore, having an understanding of these concepts allows us to understand how the internet works as a whole, and how the sites we use and interact with everyday came to be.
+
+If i where to start this project again, i would have planned to add more features at the start, rather then integrating them alnog the way. this would allow me to build the foundation around these features so i dont need to change as much later on.
--- main/app.py
+++ main/app.py
@@ -1,82 +1,153 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, abort, redirect, url_for
 from datetime import datetime, timedelta
 from alpha_vantage.timeseries import TimeSeries  
-
+from config import ALPHAVANTAGE_API_KEY
+import json #was not used
+import pandas as pd
+#importing all necassary libraries and functions
 
 app = Flask(__name__)
+#initializing the flask app
 
-def fetch_stock_data(symbol, range):
-    api_key = 'V7A683AS9GWS1JFK'
-    ts = TimeSeries(key=api_key, output_format='pandas')
+def fetch_stock_data(symbol, range): #function to fetch stock data
+    api_key = ALPHAVANTAGE_API_KEY #assigning hte API key to a variable
+    ts = TimeSeries(key=api_key, output_format='pandas') #initializing the TimeSeries function from the alpha_vantage library
 
-    # Fetch data based on the range
-    if range == '1y':
-        data, _ = ts.get_daily(symbol=symbol, outputsize='full')
-    elif range == '1m':
-        data, _ = ts.get_daily(symbol=symbol, outputsize='compact')
-        data = data.last('1M')  # Filter last 1 month
-    elif range == '1w':
-        data, _ = ts.get_daily(symbol=symbol, outputsize='compact')
-        data = data.last('7D')  # Filter last 7 days
-    elif range == '1d':
-        data, _ = ts.get_intraday(symbol=symbol, interval='5min')
-        data = data.last('1D')  # Filter last 1 day
+    try: #try block to catch any errors
+        end_date = datetime.now()
 
-    try:
-        # Extract current price and total volume
-        latest_data = data.iloc[-1]
-        current_price = latest_data['4. close']
-        total_volume = latest_data['5. volume']
-        approximate_market_cap = current_price * total_volume
-    except Exception as e:
-        current_price = total_volume = approximate_market_cap = None
-        print(f"Error calculating stock data: {e}")
+        if range == '1d':
+            # Intraday data for 1-day range with 5-minute intervals
+            data, _ = ts.get_intraday(symbol=symbol, interval='5min', outputsize='full')
+        elif range == '1w':
+            # Intraday data for 1-week range with 2-hour intervals
+            data, _ = ts.get_intraday(symbol=symbol, interval='60min', outputsize='full')
+        else:
+            # Daily data for longer ranges
+            data, _ = ts.get_daily(symbol=symbol, outputsize='full')
 
-    # Prepare dates and closing prices
-    dates = data.index.strftime('%Y-%m-%d').tolist()
-    closing_prices = data['4. close'].tolist()
+        current_price = data.iloc[0]['4. close'] #assigning the current price to a variable before reversing for accuracy
+        data = data.iloc[::-1]  # Reverse the order for chronological order
 
-    return dates, closing_prices, current_price, approximate_market_cap
+        # Calculate the start date based on the range
+        if range == '1d':
+            start_date = end_date - timedelta(days=1)
+        elif range == '1w':
+            start_date = end_date - timedelta(weeks=1)
+        elif range == '1m':
+            start_date = end_date - timedelta(days=30)
+        else:  # Default to 1 year
+            start_date = end_date - timedelta(days=365)
 
-@app.route('/')
+        processed_data = data[(data.index >= start_date) & (data.index <= end_date)] #processing the data to get the data within the range
+
+        if processed_data.empty: #if the data is empty, return None for all values
+            return None, None, None, None 
+
+        total_volume = processed_data.iloc[0].get('5. volume', 0) #assigning the total volume to a variable
+        approximate_market_cap = "${:,.2f}".format(current_price * total_volume) #calculating the market cap for the given period
+
+        dates = processed_data.index.strftime('%Y-%m-%d %H:%M:%S' if range in ['1d', '1w'] else '%Y-%m-%d').tolist() #assigning the dates to a variable
+        closing_prices = processed_data['4. close'].tolist() #assigning the closing prices to a variable
+
+    except Exception as e: #catching any errors
+        print(f"Error fetching stock data for {symbol}: {e}") #printing the error if any
+        return None, None, None, None
+  
+    return dates, closing_prices, current_price, approximate_market_cap #returning the values
+
+@app.route('/') #route for the home page
 def index():
-    return render_template('index.html')
+    return render_template('index.html')    
 
-@app.route('/DrHorton')
-def drhorton():
-    range = request.args.get('range', default='1y')
-    labels, values, current_price, market_cap = fetch_stock_data('DHI', range)
-    return render_template('drhorton.html', labels=labels, values=values, current_price=current_price, market_cap=market_cap)
 
+#routes for each stock, see DrHortons route for general description of each line as all routes are basicall the same
+
+@app.route('/DrHorton') #route for DrHorton
+def drhorton(): #function for DrHorton
+    range = request.args.get('range', default='1y') #assigning the range to a variable
+    labels, values, current_price, market_cap = fetch_stock_data('DHI', range) #calling the fetch_stock_data function and assigning the values to variables
+    
+    if None in [labels, values, current_price, market_cap]:
+        # Redirect to error page if any of the values are None
+        return redirect(url_for('error'))
+
+    return render_template('drhorton.html', labels=labels, values=values, current_price=current_price, market_cap=market_cap) #rendering the template with the values
 
 @app.route('/Amazon')
 def amazon():
     range = request.args.get('range', default='1y')
     labels, values, current_price, market_cap = fetch_stock_data('AMZN', range)
+    
+    if None in [labels, values, current_price, market_cap]:
+        # Redirect to error page if any of the values are None
+        return redirect(url_for('error'))
+
     return render_template('amazon.html', labels=labels, values=values, current_price=current_price, market_cap=market_cap)
-
-
-@app.route('/MercadoLibre')
-def mercadolibre():
-    range = request.args.get('range', default='1y')
-    labels, values, current_price, market_cap = fetch_stock_data('MELI', range)
-    return render_template('mercadolibre.html', labels=labels, values=values, current_price=current_price, market_cap=market_cap)
-
 
 @app.route('/Microsoft')
 def microsoft():
     range = request.args.get('range', default='1y')
     labels, values, current_price, market_cap = fetch_stock_data('MSFT', range)
+    
+    if None in [labels, values, current_price, market_cap]:
+        # Redirect to error page if any of the values are None
+        return redirect(url_for('error'))
+    
+    print("Current Price:", current_price)  # Add this line to log the current price, this was for testing earlier on
+    
     return render_template('microsoft.html', labels=labels, values=values, current_price=current_price, market_cap=market_cap)
-
 
 @app.route('/Nvidia')
 def nvidia():
     range = request.args.get('range', default='1y')
     labels, values, current_price, market_cap = fetch_stock_data('NVDA', range)
+    
+    if None in [labels, values, current_price, market_cap]:
+        # Redirect to error page if any of the values are None
+        return redirect(url_for('error'))
+
     return render_template('nvidia.html', labels=labels, values=values, current_price=current_price, market_cap=market_cap)
 
+@app.route('/MercadoLibre')
+def mercadolibre():
+    range = request.args.get('range', default='1y')
+    labels, values, current_price, market_cap = fetch_stock_data('MELI', range)
+    
+    if None in [labels, values, current_price, market_cap]:
+        # Redirect to error page if any of the values are None
+        return redirect(url_for('error'))
 
-if __name__ == '__main__':
+    return render_template('mercadolibre.html', labels=labels, values=values, current_price=current_price, market_cap=market_cap)
+
+@app.route('/search') #route for the search page
+def search():
+    symbol = request.args.get('stock_symbol') #assigning the stock symbol to a variable
+    if symbol: #if the symbol is not empty, redirect to the search result page
+        return redirect(url_for('search_result', symbol=symbol)) 
+    else:
+        return "Please enter a stock symbol", 400 #else return an error
+
+
+@app.route('/search_result/<symbol>') #route for the search result page
+def search_result(symbol):
+    range = request.args.get('range', default='1y')
+    labels, values, current_price, market_cap = fetch_stock_data(symbol.upper(), range)
+    
+    if None in [labels, values, current_price, market_cap]:
+        # Redirect to error page if any of the values are None
+        return redirect(url_for('error'))
+
+    
+    return render_template('search_result.html', symbol=symbol, labels=labels, values=values, current_price=current_price, market_cap=market_cap)
+
+
+@app.route('/error') #route for the error page
+def error():
+    
+    return render_template('error.html')
+
+
+if __name__ == '__main__': #running the app
     app.run(debug=True)
 
